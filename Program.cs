bcf85bd58d58795e08208283ca7a6754a5c17fe1
KillerEhida55 <aromietheanimator@gmail.com>
2024-10-30 22:22:03 +0500

    first script

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace paralo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int firstrequest = int.Parse(Console.ReadLine()!);
+        int numbers = firstrequest.ToString().Length;
+        if (numbers == 5) {
+            string secondresult = firstrequest.ToString().Substring(3);
+            string firstresult = firstrequest.ToString().Substring(0, numbers - 3);
+            char[] Reverse = firstresult.ToCharArray();
+            Array.Reverse(Reverse);
+            string result = new string(Reverse);
+            if (result == secondresult) {
+                Console.WriteLine("Да");
+            }
+            else {
+                Console.WriteLine("Нет");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Вы не ввели пятизначное число");
+        }
+    }
+}
